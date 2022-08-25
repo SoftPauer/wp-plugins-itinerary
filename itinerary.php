55862fccd6e69a3924a8d05e1a98cb3a95be0539
Tahmid <hoq78@hotmail.com>
2022-08-25 16:29:03 +0100

    itinerary reporting features

--- itinerary.php
+++ itinerary.php
@@ -13,6 +13,8 @@ add_action('admin_menu', 'itinerary_plugin_setup_menu');
 function itinerary_plugin_setup_menu()
 {
   add_menu_page('Sections', 'Sections', 'manage_options', 'itinerary-plugin-sections', 'itinerary_init_sections');
+  add_menu_page('Dashboard', 'Dashboard', 'manage_options', 'itinerary-plugin-dashboard', 'itinerary_init_dashboard');
+
 
   global $wpdb;
   $sections = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}itinerary_sections", OBJECT);
@@ -33,6 +35,11 @@ function itinerary_plugin_setup_menu()
 function itinerary_init_sections()
 {
   echo "<div section='sections' id='general-info-react'></div>";
+}
+
+function itinerary_init_dashboard()
+{
+  echo "<div section='dashboard' id='general-info-react'></div>";
 }
 
 function itinerary_ini_section($name)
@@ -68,7 +75,7 @@ add_action('admin_enqueue_scripts', function ($hook) {
 
 register_activation_hook(__FILE__, 'itinerary_install');
 
-global  $wpdb, $table_name_itinerary, $table_name_sections, $table_name_fields, $table_name_section_values, $table_name_itinerary_data;
+global  $wpdb, $table_name_itinerary, $table_name_sections, $table_name_fields, $table_name_section_values, $table_name_itinerary_data, $table_name_itinerary_channels, $table_name_reporting;
 
 $table_name_itinerary = $wpdb->prefix . 'itineraries';
 $table_name_sections = $wpdb->prefix . 'itinerary_sections';
@@ -76,11 +83,13 @@ $table_name_fields = $wpdb->prefix . 'itinerary_fields';
 $table_name_section_values = $wpdb->prefix . 'itinerary_values';
 $table_name_itinerary_data = $wpdb->prefix . 'itinerary_data';
 $table_name_itinerary_channels = $wpdb->prefix . 'itinerary_rocket_channels';
+$table_name_reporting = $wpdb->prefix . 'itinerary_reporting';
+
 
 
 function itinerary_install()
 {
-  global $wpdb, $table_name_itinerary, $table_name_sections, $table_name_fields, $table_name_section_values, $table_name_itinerary_data, $table_name_itinerary_channels;
+  global $wpdb, $table_name_itinerary, $table_name_sections, $table_name_fields, $table_name_section_values, $table_name_itinerary_data, $table_name_itinerary_channels, $table_name_reporting;
   $itinerary_db_version = '1.0';
   $charset_collate = $wpdb->get_charset_collate();
 
@@ -157,6 +166,17 @@ function itinerary_install()
     FOREIGN KEY (itinerary_id) REFERENCES $table_name_itinerary (id),
     FOREIGN KEY (section_id) REFERENCES $table_name_sections (id)
   ) $charset_collate;";
+
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+
+  $sql = "CREATE TABLE $table_name_reporting(
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    itinerary_id mediumint(9) NOT NULL,
+    passenger text NOT NULL,
+    summary text NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (itinerary_id) REFERENCES $table_name_itinerary (id),) $charset_collate;";
 
   require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
   dbDelta($sql);
@@ -344,7 +364,293 @@ add_action('rest_api_init', function () {
       ),
     )
   ));
+
+  register_rest_route('itinerary/v1', 'itineraries/updateReport/(?P<itinerary_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'update_report',
+    'args' => array(
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/addDashboardField/(?P<field_id>\d+)', array(
+    'methods' => WP_REST_Server::EDITABLE,
+    'callback' => 'add_dashboard_field',
+    'args' => array(
+      'field_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/getDashboardFields', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_dashboard_fields',
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/generateReport', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'generate_reporting_table',
+  ));
 });
+
+function get_dashboard_fields()
+{
+  global $wpdb;
+  $currentFieldQuery = $wpdb->get_results("SELECT * FROM `wp_options` WHERE `option_name` = 'dashboardFields'");
+  if ($currentFieldQuery == NULL) {
+    return array();
+  } else {
+    $currentFields = unserialize($currentFieldQuery[0]->option_value);
+    return $currentFields;
+  }
+}
+
+function add_dashboard_field(WP_REST_Request $request)
+{
+  global $wpdb;
+  $fieldId = $request['field_id'];
+  $currentFieldsQuery = $wpdb->get_results("SELECT * FROM `wp_options` WHERE `option_name` = 'dashboardFields'");
+  if ($currentFieldsQuery == NULL) {
+    $fields = array($fieldId);
+    $result = $wpdb->insert('wp_options', array('option_name' => 'dashboardFields', 'option_value' => serialize($fields)));
+    return new WP_REST_Response(array('success' => true), 200);
+  } else {
+    $currentFields = unserialize($currentFieldsQuery[0]->option_value);
+    if (in_array($fieldId, $currentFields)) {
+      return new WP_REST_Response(array('success' => false, 'message' => 'Field already exists'), 201);
+    } else {
+      array_push($currentFields, $fieldId);
+      $result = $wpdb->get_results("UPDATE `wp_options` SET `option_value` = '" . serialize($currentFields) . "' WHERE `option_name` = 'dashboardFields'");
+    }
+  }
+  return rest_ensure_response($result);
+}
+
+// function update_reporting(WP_REST_REQUEST $request){
+//   global $wpdb;
+//   $itinId = $request['itinId'];
+//   $json_data = json_decode($request->get_body());
+//   $result = $wpdb->get_results("UPDATE `wp_itineraries` SET `reporting` = '1' WHERE `id` = '" . $itinId . "'");
+//   return rest_ensure_response($result);
+// }
+
+function update_user_in_reporting($passenger_name, $passenger_object, $itin_id)
+{
+  global $wpdb, $table_name_reporting;
+  $sql = "SELECT summary FROM $table_name_reporting WHERE itinerary_id = $itin_id AND passenger = '$passenger_name'";
+  $summary = $wpdb->get_results($sql);
+
+  if ($summary == NULL) {
+    $wpdb->insert($table_name_reporting, array(
+      'itinerary_id' => $itin_id,
+      'passenger' => $passenger_name,
+      'summary' => json_encode($passenger_object)
+    ));
+  } else {
+    $wpdb->update($table_name_reporting, array(
+      'summary' => json_encode($passenger_object)
+    ), array(
+      'itinerary_id' => $itin_id,
+      'passenger' => $passenger_name
+    ));
+  }
+}
+
+function find_root_section($field)
+{
+  global $wpdb, $table_name_fields;
+  if ($field->parent == NULL) {
+    return $field;
+  } else {
+    $parent = $wpdb->get_results("SELECT * FROM $table_name_fields WHERE id = $field->parent");
+    return find_root_section($parent[0]);
+  }
+}
+
+function get_root_key($field)
+{
+  $root_node = find_root_section($field);
+  $root_type_props = json_decode($root_node->type_properties, true);
+
+  if ($root_type_props == NULL || $root_type_props['json_key'] == "") {
+    return str_replace(' ', '_', $root_node->field_name);
+  } else {
+    return $root_type_props["json_key"];
+  }
+}
+
+function generate_reporting_table(WP_REST_Request $request)
+{
+  global $wpdb, $table_name_reporting, $table_name_fields, $table_name_section_values;
+  //delete table entries 
+  $wpdb->query("DELETE FROM $table_name_reporting");
+
+  //get all itineraries
+  $itineraries = $wpdb->get_results("SELECT id FROM wp_itineraries");
+
+
+  //get all fields with reporting enabled
+  $sql = 'SELECT * FROM ' . $table_name_fields . ' WHERE type_properties LIKE ' . "'" . '%"showOnDashboard":true%' . "'";
+
+  // $fields_with_reporting = $wpdb->get_results("SELECT * FROM " . $table_name_fields . ` WHERE type_properties LIKE '%"showOnDashboard":true%';`);
+  $fields_with_reporting = $wpdb->get_results($sql);
+
+  foreach ($itineraries as $itin) {
+    $tableDict = array();
+    foreach ($fields_with_reporting as $field) {
+      $root_key = get_root_key($field);
+      [$parent_field, $parent_json_key, $field_json_key] = get_field_parent($field, $field->section);
+      $data = $wpdb->get_results("SELECT value FROM " . $table_name_section_values . " WHERE itinerary = " . $itin->id . " AND section =" . $field->section);
+      $values = json_decode($data[0]->value);
+      // $values = $values->$root_key;
+      foreach ($values as $value) {
+        foreach ($value as $section_value) {
+          if ($parent_json_key != "" && isset($section_value->$parent_json_key)) {
+            $passenger_value = $section_value->$parent_json_key;
+            //passenger values in this case are stored in arrays
+
+            // error_log("passenger: " . print_r($passenger_value, true));
+            // error_log(print_r($section_value, true));
+            foreach ($passenger_value as $passenger) {
+              if (is_object($passenger)) {
+
+                $passenger_data = $passenger->{$field_json_key};
+                if (is_array($passenger_data)) {
+                  foreach ($passenger_data as $passenger_value) {
+                    add_to_array($tableDict, $passenger_value, $section_value, $root_key);
+                  }
+                } else {
+                  $passenger_data = strval($passenger_data);
+                  add_to_array($tableDict, $passenger_data, $section_value, $root_key);
+                }
+              }
+            }
+          } else {
+            //passenger values in this case are stored in objects
+            $passenger_value = $section_value->$field_json_key;
+            if (is_array($passenger_value)) {
+              foreach ($passenger_value as $passenger) {
+                add_to_array($tableDict, $passenger, $section_value, $root_key);
+              }
+            } else {
+              add_to_array($tableDict, $passenger_value, $section_value, $root_key);
+            }
+          }
+        }
+      }
+    }
+
+    foreach ($tableDict as $passenger_name => $passenger_data) {
+      update_user_in_reporting($passenger_name, $passenger_data, $itin->id);
+    }
+  }
+
+
+
+
+  return rest_ensure_response(($tableDict));
+}
+
+function add_to_array(array &$tableDict, $passenger_data, $section_value, $root_key)
+{
+  if (!isset($tableDict[$passenger_data])) {
+    $tableDict[$passenger_data] = array();
+  }
+  $user_obj = &$tableDict[$passenger_data];
+  if (!isset($user_obj[$root_key])) {
+    $user_obj[$root_key] = array();
+  }
+  array_push($user_obj[$root_key], $section_value);
+  // array_push($user_entry[$root_key], $section_value);
+  $tableDict[$passenger_data] = $user_obj;
+}
+
+function get_field_parent($field, $section_id)
+{
+  global $wpdb, $table_name_fields;
+  if ($field->parent != null) {
+  }
+  $parent_field_to_user = $wpdb->get_results("SELECT * FROM $table_name_fields WHERE id = $field->parent AND section = $section_id");
+  $parent_field_to_user = $parent_field_to_user[0];
+  //unpack type props for field to get json_key 
+  $user_field_type_props = json_decode($field->type_properties, true);
+  $user_field_json_key = $user_field_type_props['json_key'];
+  $parent_field_type_props = json_decode($parent_field_to_user->type_properties, true);
+  $parent_field_json_key = $parent_field_type_props['json_key'];
+
+  return [$parent_field_to_user, $parent_field_json_key, $user_field_json_key];
+}
+
+function get_users_from_val($val, $user_fields, $section_id, $itinerary)
+{
+  if (!is_array($user_fields)) {
+    return array();
+  }
+  global $wpdb, $table_name_fields;
+  $data = json_decode($val, true);
+  $data_keys = array_keys($data);
+  $passengers = array();
+  foreach ($user_fields as $field) {
+    $parent_field_to_user = $wpdb->get_results("SELECT * FROM " . $table_name_fields . " WHERE id = " . $field->parent . " AND section = " . $section_id);
+    $parent_field_to_user = $parent_field_to_user[0];
+    //unpack type props for field to get json_key 
+    $user_field_type_props = json_decode($field->type_properties, true);
+    $user_field_json_key = $user_field_type_props['json_key'];
+    $parent_field_type_props = json_decode($parent_field_to_user->type_properties, true);
+    $parent_field_json_key = $parent_field_type_props['json_key'];
+    //check if the parent field has any json key if it doesnt then its parent node to structure
+    if ($parent_field_json_key == null) {
+      foreach ($data[$data_keys[0]] as $section_item) {
+        if (array_key_exists($user_field_json_key, $section_item) && is_array($section_item[$user_field_json_key])) {
+          $users = $section_item[$user_field_json_key];
+          // function to update the current user/list in the reporting table
+          foreach ($users as $user) {
+            update_user_in_reporting($user, $section_item, $itinerary);
+          }
+        }
+      }
+
+      foreach ($data[$data_keys[0]] as $data_rows) {
+        error_log("data rows: " . print_r($data_rows[$user_field_json_key], true));
+        if (is_array($data_rows[$user_field_json_key])) {
+          $passenger_names = $data_rows[$user_field_json_key];
+          foreach ($passenger_names as $passenger_name) {
+            array_push($passengers, $passenger_name);
+          }
+        } else {
+          foreach ($data as $field) {
+            if ($field[$user_field_json_key]) {
+              if (count($users) == 0) {
+                $users = $field[$user_field_json_key];
+              }
+              $users = array_merge($passengers, $field[$user_field_json_key]);
+            }
+          }
+        }
+      }
+    }
+  }
+  error_log("use2rs: " . print_r($users, true));
+
+  return $users;
+}
+
+function update_reporting($val, $id, $section, $itinerary)
+{
+  global $wpdb, $table_name_sections, $table_name_fields, $table_name_reporting;
+  $sql = "SELECT * FROM " . $table_name_fields . " WHERE type_properties LIKE '%showOnDashboard%' AND section = " . $section;
+  $fields_with_reporting = $wpdb->get_results($sql);
+  $users = get_users_from_val($val, $fields_with_reporting, $section, $itinerary);
+  error_log("users: " . print_r($users, true));
+}
+
 
 
 function create_rocket_channel(WP_REST_Request $request)
@@ -565,7 +871,11 @@ function delete_section($data)
 function get_all_fields($data)
 {
   global $wpdb, $table_name_fields;
-  $results = $wpdb->get_results("SELECT * FROM {$table_name_fields} WHERE section = {$data['section_id']}", OBJECT);
+  if ($data["section_id"] != 0) {
+    $results = $wpdb->get_results("SELECT * FROM {$table_name_fields} WHERE section = {$data['section_id']}", OBJECT);
+  } else {
+    $results = $wpdb->get_results("SELECT * FROM {$table_name_fields}", OBJECT);
+  }
   return rest_ensure_response($results);
 }
 
@@ -627,11 +937,14 @@ function itin_delete_field($data)
  */
 function get_all_section_values($data)
 {
-  $results = get_section_value($data['section_id'], $data['itinerary_id']);
-  if (empty($results)) {
-    return rest_ensure_response(null);
+  if ($data['section_id'] != 0) {
+    $results = get_section_value($data['section_id'], $data['itinerary_id']);
+    if (empty($results)) {
+      return rest_ensure_response(null);
+    }
+    return rest_ensure_response($results[0]);
   }
-  return rest_ensure_response($results[0]);
+  return rest_ensure_response(get_itinerary_values($data['itinerary_id']));
 }
 
 /**
@@ -664,6 +977,7 @@ function create_update_value($value)
     $sql = "UPDATE {$table_name_section_values} SET value = %s  WHERE id = '{$results[0]->id}'";
     $sql = $wpdb->prepare($sql,  $val);
     $data = ['updated' => $wpdb->query($sql)];
+    update_reporting($val, $results[0]->id, $value->section, $value->itinerary);
     return json_encode($data);
   } else {
     $sql = "INSERT INTO 
@@ -752,6 +1066,18 @@ function get_section_value($section_id, $itinerary_id)
      AND itinerary = {$itinerary_id} ",
     OBJECT
   );
+  return $results;
+}
+
+function get_itinerary_values($itinerary_id)
+{
+  global $wpdb, $table_name_section_values;
+  $results = $wpdb->get_results(
+    "SELECT * FROM {$table_name_section_values} 
+    WHERE itinerary = {$itinerary_id} ",
+    OBJECT
+  );
+  error_log(json_encode($results));
   return $results;
 }
 
