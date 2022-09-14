30a72f9638633a022893f9c89a9d450c1ed1885c
alexp.powell <alexp.powell@softpauer.com>
2022-09-14 06:41:20 +0100

    refactoring php

---
+++ lib/db_creation.php
@@ -0,0 +1,106 @@
+<?php
+global  $wpdb, $table_name_itinerary, $table_name_sections, $table_name_fields, $table_name_section_values, $table_name_itinerary_data, $table_name_costings, $table_name_itinerary_channels, $table_name_reporting;
+
+$table_name_itinerary = $wpdb->prefix . 'itineraries';
+$table_name_sections = $wpdb->prefix . 'itinerary_sections';
+$table_name_fields = $wpdb->prefix . 'itinerary_fields';
+$table_name_section_values = $wpdb->prefix . 'itinerary_values';
+$table_name_itinerary_data = $wpdb->prefix . 'itinerary_data';
+$table_name_itinerary_channels = $wpdb->prefix . 'itinerary_rocket_channels';
+$table_name_reporting = $wpdb->prefix . 'itinerary_reporting';
+$table_name_costings = $wpdb->prefix . 'itinerary_costings';
+
+function itinerary_install()
+{
+    global $wpdb, $table_name_itinerary, $table_name_sections, $table_name_fields, $table_name_section_values, $table_name_itinerary_data, $table_name_itinerary_channels, $table_name_reporting, $table_name_costings;
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+    $itinerary_db_version = '1.0';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name_itinerary (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        time_created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        time_updated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        name tinytext NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+    dbDelta($sql);
+
+    $sql = "CREATE TABLE $table_name_sections (
+          id mediumint(9) NOT NULL AUTO_INCREMENT,
+          time_created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+          time_updated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+          properties text,
+          name tinytext NOT NULL,
+          PRIMARY KEY  (id)
+      ) $charset_collate;";
+    dbDelta($sql);
+
+    $sql = "CREATE TABLE $table_name_fields (
+          id mediumint(9) NOT NULL AUTO_INCREMENT,
+          section mediumint(9) NOT NULL,
+          position mediumint(9) NOT NULL,
+          field_type tinytext NOT NULL,
+          field_name tinytext NOT NULL,
+          parent mediumint(9),
+          type_properties text,
+          PRIMARY KEY  (id)
+      ) $charset_collate;";
+    dbDelta($sql);
+
+    $sql = "CREATE TABLE $table_name_section_values (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    section mediumint(9)  NOT NULL,
+    itinerary mediumint(9) NOT NULL,
+    value text,
+    PRIMARY KEY  (id)
+  ) $charset_collate;";
+    dbDelta($sql);
+
+    $sql = "CREATE TABLE $table_name_itinerary_data (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    itinerary_id mediumint(9) NOT NULL,
+    time_updated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+    json_data text  NOT NULL,
+    PRIMARY KEY  (id),
+    FOREIGN KEY(itinerary_id) REFERENCES $table_name_itinerary(id)
+  ) $charset_collate;";
+    dbDelta($sql);
+
+    add_option('itinerary_db_version', $itinerary_db_version);
+
+    $sql = "CREATE TABLE $table_name_itinerary_channels (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    itinerary_id mediumint(9) NOT NULL,
+    section_id mediumint(9) NOT NULL,
+    json_data text NOT NULL,
+    channel_id text,
+    PRIMARY KEY (id),
+    FOREIGN KEY (itinerary_id) REFERENCES $table_name_itinerary (id),
+    FOREIGN KEY (section_id) REFERENCES $table_name_sections (id)
+  ) $charset_collate;";
+    dbDelta($sql);
+
+    $sql = "CREATE TABLE $table_name_reporting(
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    itinerary_id mediumint(9) NOT NULL,
+    passenger text NOT NULL,
+    summary text NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (itinerary_id) REFERENCES $table_name_itinerary (id)
+  ) $charset_collate;";
+    dbDelta($sql);
+
+    $sql = "CREATE TABLE $table_name_costings (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    itinerary_id mediumint(9) NOT NULL,
+    section_id mediumint(9) NOT NULL,
+    listKey text NOT NULL,
+    costing text,
+    PRIMARY KEY (id),
+    FOREIGN KEY (itinerary_id) REFERENCES $table_name_itinerary (id),
+    FOREIGN KEY (section_id) REFERENCES $table_name_sections (id)
+  ) $charset_collate;";
+    dbDelta($sql);
+}
---
+++ lib/rest_api.php
@@ -0,0 +1,266 @@
+<?php
+// API setup
+// %s (string)
+// %d (integer)
+// %f (float)
+//  Itineraries
+add_action('rest_api_init', function () {
+  register_rest_route('itinerary/v1', 'itineraries', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_all_itineraries',
+  ));
+  register_rest_route('itinerary/v1', 'itineraries/create', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'create_new_itinerary',
+  ));
+  register_rest_route('itinerary/v1', 'itineraries/delete/(?P<itinerary_id>\d+)', array(
+    'methods' => WP_REST_Server::DELETABLE,
+    'callback' => 'delete_itinerary',
+    'args' => array(
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  //costings
+   register_rest_route('itinerary/v1', 'costings/(?P<itinerary_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_all_costings',
+    'args' => array(
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+  register_rest_route('itinerary/v1', 'costings/create', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'create_new_costing',
+  ));
+  register_rest_route('itinerary/v1', 'costings/delete/(?P<costing_id>\d+)', array(
+    'methods' => WP_REST_Server::DELETABLE,
+    'callback' => 'delete_costing',
+    'args' => array(
+      'costing_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  //sections
+  register_rest_route('itinerary/v1', 'sections', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_all_sections',
+  ));
+  register_rest_route('itinerary/v1', 'sections/create', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'create_new_section',
+  ));
+  register_rest_route('itinerary/v1', 'sections/delete/(?P<section_id>\d+)', array(
+    'methods' => WP_REST_Server::DELETABLE,
+    'callback' => 'delete_section',
+    'args' => array(
+      'section_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+  //fields
+  register_rest_route('itinerary/v1', 'fields/(?P<section_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_all_fields',
+    'args' => array(
+      'section_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+  register_rest_route('itinerary/v1', 'fields/create', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'create_new_field',
+  ));
+  register_rest_route('itinerary/v1', 'fields/delete/(?P<field_id>\d+)', array(
+    'methods' => WP_REST_Server::DELETABLE,
+    'callback' => 'itin_delete_field',
+    'args' => array(
+      'field_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  //field values
+  register_rest_route('itinerary/v1', 'values/(?P<itinerary_id>\d+)/(?P<section_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_all_section_values',
+    'args' => array(
+      'section_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'values/createOrUpdate', array(
+    'methods' => WP_REST_Server::EDITABLE,
+    'callback' => 'create_new_section_value',
+  ));
+
+  register_rest_route('itinerary/v1', 'values/delete/(?P<value_id>\d+)', array(
+    'methods' => WP_REST_Server::DELETABLE,
+    'callback' => 'delete_value',
+    'args' => array(
+      'value_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'values/copyLast/(?P<itin_id>\d+)', array(
+    'methods' => WP_REST_Server::EDITABLE,
+    'callback' => 'copy_values_from_last_itin',
+    'args' => array(
+      'itin_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'values/copyItinerary/(?P<itin_id>\d+)', array(
+    'methods' => WP_REST_Server::EDITABLE,
+    'callback' => 'copy_all_values_from_selected_itinerary',
+    'args' => array(
+      'itin_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/updateApp', array(
+    'methods' => WP_REST_Server::EDITABLE,
+    'callback' => 'update_entry_in_db',
+    'args' => array(
+      'itinId' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'raceMap', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_race_map',
+  ));
+
+  register_rest_route('itinerary/v1', 'data/(?P<itin_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_itin_data',
+    'args' => array(
+      'itin_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'ical/(?P<usertoken>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_ical_for_user',
+    'args' => array(
+      'usertoken' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return true;
+        }
+      ),
+    )
+  ));
+  register_rest_route('itinerary/v1', 'rocketChannel/create', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'create_rocket_channel',
+  ));
+
+  register_rest_route('itinerary/v1', 'rocketChannel/getChannels/(?P<itinerary_id>\d+)/(?P<section_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_rocket_channels',
+    'args' => array(
+      'section_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/updateReport/(?P<itinerary_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'update_report',
+    'args' => array(
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/getDashboardFields', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'get_dashboard_fields',
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/generateReport/(?P<itinerary_id>\d+)', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'generate_reporting_table',
+    'args' => array(
+      'itinerary_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+  register_rest_route('itinerary/v1', 'itineraries/addDashboardField/(?P<field_id>\d+)', array(
+    'methods' => WP_REST_Server::EDITABLE,
+    'callback' => 'add_dashboard_field',
+    'args' => array(
+      'field_id' => array(
+        'validate_callback' => function ($param, $request, $key) {
+          return is_numeric($param);
+        }
+      ),
+    )
+  ));
+
+});
