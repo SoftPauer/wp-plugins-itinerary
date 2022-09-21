a7b93db0ee74548852cab429b7f0ec62752c5cd3
andriusm <andriusm@softpauer.com>
2022-09-21 10:45:34 +0100

    empty line breaks the universe

--- itinerary.php
+++ itinerary.php
@@ -3,7 +3,7 @@
 Plugin Name: Itinerary plugin
 Description: Plugin to control itinerary 
 Author: Andrius Murauskas
-Version: 1.2.8
+Version: 1.2.9
 GitHub Plugin URI: https://github.com/SoftPauer/wp-plugins-itinerary
 */
 require_once __DIR__.'/lib/rest_api.php';
