55b2d4937d4968865fac53f16df468c8b813e54b
Ellie Harris <ellieh@softpauer.com>
2022-09-27 09:50:43 +0100

    version up

--- itinerary.php
+++ itinerary.php
@@ -3,7 +3,7 @@
 Plugin Name: Itinerary plugin
 Description: Plugin to control itinerary 
 Author: Andrius Murauskas
-Version: 1.2.15
+Version: 1.2.16
 
 GitHub Plugin URI: https://github.com/SoftPauer/wp-plugins-itinerary
 */
