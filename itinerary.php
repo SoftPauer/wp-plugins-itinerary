021a372ae12b52abfbdead237c3f96df55e1d571
fernandot-softpauer <110815942+fernandot-softpauer@users.noreply.github.com>
2022-12-01 11:54:42 +0000

    reverse fix

--- itinerary.php
+++ itinerary.php
@@ -3,7 +3,7 @@
 Plugin Name: Itinerary plugin
 Description: Plugin to control itinerary 
 Author: Andrius Murauskas
-Version: 1.2.53
+Version: 1.2.54
 
 GitHub Plugin URI: https://github.com/SoftPauer/wp-plugins-itinerary
 */
