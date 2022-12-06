c922552d78c8aefea9fd22e48f7df4b1da29e7a3
andriusm <andriusm@softpauer.com>
2022-12-06 07:58:05 +0000

    using get_option

--- lib/bl/config.php
+++ lib/bl/config.php
@@ -2,8 +2,6 @@
 
 function get_all_config()
 {
-    global $wpdb;
-    $isWizardComplete = $wpdb->get_results("SELECT * FROM `wp_options` WHERE `option_name` = 'isWizardComplete'");
-    $isWizardComplete = $isWizardComplete[0]??false;
+  $isWizardComplete = get_option('isWizardComplete');
   return rest_ensure_response(array('isWizardComplete' =>  $isWizardComplete));
 }
