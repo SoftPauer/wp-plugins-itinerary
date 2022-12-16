743ed23015dacdcfde13ed8c1cf74821ab14c4d0
fernandot-softpauer <110815942+fernandot-softpauer@users.noreply.github.com>
2022-12-16 12:03:44 +0000

    v arriba

--- itinerary.php
+++ itinerary.php
@@ -3,7 +3,7 @@
 Plugin Name: Itinerary plugin
 Description: Plugin to control itinerary 
 Author: Andrius Murauskas
-Version: 1.3.57
+Version: 1.3.58
 GitHub Plugin URI: https://github.com/SoftPauer/wp-plugins-itinerary
 */
 require_once __DIR__ . '/lib/rest_api.php';
